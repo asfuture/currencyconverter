295ae2743728c823ea7add73dbdb6a0833d89ebd
Alex Ferreira <asfuture2019@gmail.com>
2024-06-19 23:21:36 -0300

    Dolár Canadense ok
    Interface ok
    Services ok
    Mudar cor ok
    Atualização a cada  3 minutos ok

---
+++ src/app/shared/model/cotacao.ts
@@ -0,0 +1,19 @@
+export interface Cotacao {
+    code: string;
+    codein: string;
+    name: string;
+    high: string;
+    low: string;
+    varBid: string;
+    pctChange: string;
+    bid: string;
+    ask: string;
+    timestamp: string;
+    create_date: string;
+}
+
+export interface CotacaoSimplificada {
+    ask: string;
+    pctChange: string;
+    timestamp: string;
+}
