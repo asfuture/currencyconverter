7aa3480ac421ad7406c1b24cb0f163e93a5e4d34
Alex Ferreira <asfuture2019@gmail.com>
2024-06-20 10:26:01 -0300

    Atualização automática a cada 3 minutos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DolarCanadenseComponent } from './shared/views/dolar-canadense/dolar-canadense.component';
+import { PesoArgentinoComponent } from './shared/views/peso-argentino/peso-argentino.component';
+import { LibraEsterlinaComponent } from './shared/views/libra-esterlina/libra-esterlina.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component: DolarCanadenseComponent
+    },
+    {
+        path:'', component: PesoArgentinoComponent
+    },
+    {
+        path:'', component: LibraEsterlinaComponent
+    }
+];
